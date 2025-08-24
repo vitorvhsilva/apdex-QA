0fe3f7ac081df9477e33746f5e9b472e85ca1b75
vitorvhsilva <dndragonbr@gmail.com>
2025-08-24 14:43:02 -0300

    validando quantidade de amostras com o RM

--- src/test/java/org/example/ApdexTest.java
+++ src/test/java/org/example/ApdexTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApdexTest {
     private Apdex apdex;
+    private final Integer rm = 558961;
 
     @BeforeEach
     public void setUp() {
@@ -17,35 +18,45 @@ public class ApdexTest {
     @Test
     public void calcularApdexExcelente() {
         Double score = apdex.calcularApdex(535000, 20000, 3961);
-        assertEquals(0.97, score, 0.01);
-        assertEquals(ClassificacaoApdex.EXCELENTE, apdex.classificarApdex(score));
+
+        assertEquals(0.97, score, 0.01); //valida score
+        assertEquals(ClassificacaoApdex.EXCELENTE, apdex.classificarApdex(score)); //valida classificacao
+        assertEquals(535000 + 20000 + 3961, rm); //valida total de amostras com o rm
     }
 
     @Test
     public void calcularApdexBom() {
         Double score = apdex.calcularApdex(450000, 60000, 48961);
-        assertEquals(0.85, score, 0.01);
-        assertEquals(ClassificacaoApdex.BOM, apdex.classificarApdex(score));
+
+        assertEquals(0.85, score, 0.01); //valida score
+        assertEquals(ClassificacaoApdex.BOM, apdex.classificarApdex(score)); //valida classificacao
+        assertEquals(450000 + 60000 + 48961, rm); //valida total de amostras com o rm
     }
 
     @Test
     public void calcularApdexRazoavel() {
         Double score = apdex.calcularApdex(360000, 80000, 118961);
-        assertEquals(0.72, score, 0.01);
-        assertEquals(ClassificacaoApdex.RAZOAVEL, apdex.classificarApdex(score));
+
+        assertEquals(0.72, score, 0.01); //valida score
+        assertEquals(ClassificacaoApdex.RAZOAVEL, apdex.classificarApdex(score)); //valida classificacao
+        assertEquals(360000 + 80000 + 118961, rm); //valida total de amostras com o rm
     }
 
     @Test
     public void calcularApdexRuim() {
         Double score = apdex.calcularApdex(300000, 70000, 188961);
-        assertEquals(0.59, score, 0.01);
-        assertEquals(ClassificacaoApdex.RUIM, apdex.classificarApdex(score));
+
+        assertEquals(0.59, score, 0.01); //valida score
+        assertEquals(ClassificacaoApdex.RUIM, apdex.classificarApdex(score)); //valida classificacao
+        assertEquals(300000 + 70000 + 188961, rm); //valida total de amostras com o rm
     }
 
     @Test
     public void calcularApdexInaceitavel() {
         Double score = apdex.calcularApdex(200000, 60000, 298961);
-        assertEquals(0.41, score, 0.01);
-        assertEquals(ClassificacaoApdex.INACEITAVEL, apdex.classificarApdex(score));
+        
+        assertEquals(0.41, score, 0.01); //valida score
+        assertEquals(ClassificacaoApdex.INACEITAVEL, apdex.classificarApdex(score)); //valida classificacao
+        assertEquals(200000 + 60000 + 298961, rm); //valida total de amostras com o rm
     }
 }
